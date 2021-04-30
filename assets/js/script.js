37318f56f7ec2b1a1f9500db342078f0fa4565ef
Joshua Abeyta <mentaltemple@gmail.com>
2021-04-29 21:57:52 -0600

    added current date to header with jquery function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
+//read from local storage
+//assign values to schedule
+
 // current day is displayed at the top of the calendar
+
+//each timeblock is color coded to indicate whether it is in the past, present, or future
+
+//WHEN I click into a timeblock, THEN I can enter an event
+
+//on save button click,
+
+//read the text input from the text area (jQuery)
+
+//save to local storage
+
+$(document).ready(function () {
+  //write all of our jQuery inside this function
+
+  //VARIABLE DECLARATIONS
+  var currentDay = $("#currentDay");
+  currentDay.text(moment().format("dddd, MMMM Do YYYY"));
+
+  //display the current date in that element
+
+  //display the date in the header
+  //first, grab a ref to teh element where we want the time displayed
+
+  //FUNCTIONS
+
+  //EVENT HANDLERS
+});
